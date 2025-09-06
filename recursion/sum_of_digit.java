26b9026d8eacbd7ac51fd5b4630e7dfb1cd098da
Aadithya Nayak V <aadithyanayakv@gmail.com>
2025-09-06 20:22:08 +0530

    added sum of digit

--- recursion/sum_of_digit.java
+++ recursion/sum_of_digit.java
@@ -1,5 +1,15 @@
-package recursion;
 
 public class sum_of_digit {
-    
+    static int sum=0;
+    static int summ(int n){
+        if(n==0)return 0;
+        int rem=n%10;
+        
+        
+        return rem+ summ(n/10);
+
+    }
+    public static void main(String[] args) {
+        System.out.println(summ(1349));
+    }
 }
