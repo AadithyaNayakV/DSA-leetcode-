b1c9bd64af4e93d12b7c0025aaf7c32035d738d1
Aadithya Nayak V <aadithyanayakv@gmail.com>
2026-03-28 21:14:40 +0530

    dice probelm fir particulr target

--- recursion/number_of_diceroll_with_target.java
+++ recursion/number_of_diceroll_with_target.java
@@ -3,14 +3,19 @@ import java.util.ArrayList;
 public class number_of_diceroll_with_target {
 static ArrayList<String> list=new ArrayList<>();
     public static void main(String[] args) {
-        helper(0,4,4);
+        helper("",4);
          for(String s:list){
             System.out.println(s);
         }
     }
-    void helper(String p,int un,int target){
-        if(p==target){
+    static void helper(String p,int target){
+        if(target==0){
             list.add(p);
+            System.out.println(p);
+            return ;
+        }
+        for(int i=1;i<=6&&i<=target;i++){
+            helper(p+i,target-i);
         }
     }
 }
