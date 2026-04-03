b001a01ff46a8913dec96505a015d0866a4a3b00
Aadithya Nayak V <aadithyanayakv@gmail.com>
2026-04-03 18:42:20 +0530

    maze

---
+++ recursion/DP/source_dest.java
@@ -0,0 +1,18 @@
+public class source_dest {
+    public static void main(String[] args) {
+      //  System.out.println(helper("",3,3));
+      helper("",3,3);
+      
+    }
+   static void helper(String path,int s,int d){
+      if(s<1||d<1)return;
+        if(s==1&&d==1){System.out.println(path);
+   return ;}
+
+        
+
+         helper(path+"D",s-1, d);
+          helper(path+"R",s, d-1);
+          
+    }
+}
